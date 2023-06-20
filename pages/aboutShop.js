a6de9973a768d8fc7d06f865d301a12fe618b908
AnnVinogradova <saikotakaki@gmail.com>
2023-06-20 12:14:08 +0300

    переустановила node и yarm

---
+++ pages/aboutShop.js
@@ -0,0 +1,9 @@
+
+export default function AboutShop() {
+	return <>
+		<ul class="breadcrumb">
+			<li><a href="main">Главная</a></li>
+			<li><a href="catalog">О магазине</a></li>
+		</ul>
+	</>;
+}
