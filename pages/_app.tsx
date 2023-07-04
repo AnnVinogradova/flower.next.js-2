331a57bd1931952b2b69843bcbe7e0b402009833
AnnVinogradova <saikotakaki@gmail.com>
2023-07-04 15:03:12 +0300

    AboutDelivery Chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Header from '../components/Header/Header.js';
 import Footer from '../components/Footer/Footer.js';
 import Instagram from '../components/Instagram/Instagram.js';
